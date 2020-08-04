f36bbc249c79c34d9d164c2a24464fb598be6f1e
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-05 01:04:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿/*using System;
+
+namespace ArrayParity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 }; // 6 = 2 + 4
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i += 1)
+            {
+                if (i % 2 != 0)
+                {
+                    sum += arr[i];
+                }
+            }
+            Console.WriteLine("Сумма: " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}*/
+
+/*using System;
+
+namespace ArrayParity
+{
+    class Program
+    {
+        static void Main(string[] args) // 9 = 1 + 3 + 5
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i += 1)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += arr[i];
+                }
+            }
+            Console.WriteLine("Сумма: " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}*/
